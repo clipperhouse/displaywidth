b5acb11026b0aa09c8a843c08eb6f867e002190e
Matt Sherman <mwsherman@gmail.com>
2025-10-08 18:14:21 -0400

    Add test data

---
+++ internal/testdata/testdata.go
@@ -0,0 +1,28 @@
+package testdata
+
+import (
+	"os"
+	"path/filepath"
+	"runtime"
+)
+
+func InvalidUTF8() ([]byte, error) {
+	return load("UTF-8-test.txt")
+}
+
+func Sample() ([]byte, error) {
+	return load("sample.txt")
+}
+
+func TestCases() ([]byte, error) {
+	return load("test_cases.txt")
+}
+
+func load(filename string) ([]byte, error) {
+	// Get the directory of this source file
+	_, currentFile, _, _ := runtime.Caller(0)
+	dir := filepath.Dir(currentFile)
+	path := filepath.Join(dir, filename)
+
+	return os.ReadFile(path)
+}
