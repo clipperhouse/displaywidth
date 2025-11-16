f9dadd32525c86384928060d8f3fead5cff72c9c
Matt Sherman <mwsherman@gmail.com>
2025-11-16 11:33:44 -0500

    typos

--- tables.go
+++ tables.go
@@ -12,7 +12,7 @@ var propertyWidths = [5]int{
 // asciiWidths is a lookup table for single-byte character widths. Printable
 // ASCII characters have width 1, control characters have width 0.
 //
-// It is intended for valid single-byte valid UTF-8, which means <128.
+// It is intended for valid single-byte UTF-8, which means <128.
 //
 // If you look up an index >= 128, that is either:
 //   - invalid UTF-8, or
@@ -45,14 +45,15 @@ var asciiWidths = [256]int8{
 }
 
 // asciiProperties is a lookup table for single-byte character properties.
-// It is intended for valid single-byte valid UTF-8, which means <128.
+// It is intended for valid single-byte UTF-8, which means <128.
 //
 // If you look up an index >= 128, that is either:
 //   - invalid UTF-8, or
 //   - a multi-byte UTF-8 sequence, in which case you should be operating on
 //     the grapheme cluster, and not using this table
 //
-// We will return a default value of _Default in those cases, so as not to panic.
+// We will return a default value of _Default in those cases, so as not to
+// panic.
 var asciiProperties = [256]property{
 	// Control characters (0x00-0x1F): _Zero_Width
 	_Zero_Width, _Zero_Width, _Zero_Width, _Zero_Width, _Zero_Width, _Zero_Width, _Zero_Width, _Zero_Width,
